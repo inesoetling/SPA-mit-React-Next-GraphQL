dd54e55f121acbb9ad7a0b3e89d67aeeda26aa57
dilaradg <dilara.dg@outlook.com>
2025-12-23 01:37:56 +0100

    chore: Mutations entfernt (gehört in dilara-create)

--- frontend/src/graphql/mutations.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from '@apollo/client';
-
-export const CREATE_BOOK = gql`
-    mutation CreateBook($input: BuchInput!) {
-        create(input: $input) {
-            id
-        }
-    }
-`;
