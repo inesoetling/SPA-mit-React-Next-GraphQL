f48d7ed62bac7f82ae58c0eb6c4063b61a998063
Ines Oetling <ines.oetling@outlook.com>
2025-12-28 00:01:28 +0100

    chore: add image

--- frontend/src/app/login/index.tsx
+++ frontend/src/app/login/index.tsx
@@ -1,8 +1,13 @@
+import Image from 'react-bootstrap/Image';
 import LoginForm from "./LoginForm";
 
 const Index = () => {
   return(
     <main>
+      <Image src="/BookLogo.png"
+      width={300}
+      height={300}
+      alt="Logo" rounded  />
       <LoginForm />
     </main>
   );
