e444ce1b27e1386fb59230aa0791ae9d57cd4bf6
Ines Oetling <ines.oetling@outlook.com>
2025-12-27 20:51:06 +0100

    chore: add LoginForm component

---
+++ frontend/src/app/login/LoginForm.tsx
@@ -0,0 +1,11 @@
+import Form from 'react-bootstrap/Form';
+const LoginForm = () => {
+  return (
+    <>
+      <Form.Label>Username</Form.Label>
+      <Form.Label>Password</Form.Label>
+    </>
+  );
+};
+
+export default LoginForm;
