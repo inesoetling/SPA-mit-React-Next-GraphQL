f75149dc014a5122e4b371de982af59dafa3886a
Ines Oetling <ines.oetling@outlook.com>
2025-12-28 01:31:50 +0100

    style: UI improvements

--- frontend/src/app/login/index.tsx
+++ frontend/src/app/login/index.tsx
@@ -4,11 +4,17 @@ import "../globals.css";
 
 const Index = () => {
   return(
-    <main  className="centered-page">
+    <main className="centered-page">
       <Image src="/BookLogo.png"
-      width={300}
-      height={300}
+      width={200}
+      height={200}
       alt="Logo" rounded  />
+
+      <h1>BookManager</h1>
+      <h5>Admin Portal</h5>
+      <br></br>
+      <h2>Login</h2>
+
       <LoginForm />
     </main>
   );
