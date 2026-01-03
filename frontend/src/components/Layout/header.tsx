44161855152ecf3982a559634b12db40bc64b9a7
Ines Oetling <ines.oetling@outlook.com>
2026-01-03 20:44:12 +0100

    chore: Toggle und Collapse hinzufügen

--- frontend/src/components/Layout/header.tsx
+++ frontend/src/components/Layout/header.tsx
@@ -10,16 +10,20 @@ const Header = () => {
     <Navbar bg="primary" variant="dark" expand="lg">
         <Navbar.Brand as={Link} href="/">
           <Image src="/BookLogo.png"
-          width={40}
-          height={40}
-          alt="Logo" />
+            width={40}
+            height={40}
+            alt="Logo"/>
           BookManager
         </Navbar.Brand>
-        <Nav>
-          <Nav.Link as={Link} href="/books">Bücher</Nav.Link>
-          <Nav.Link as={Link} href="/create">Neu</Nav.Link>
-        </Nav>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} href="/">Home</Nav.Link>
+            <Nav.Link as={Link} href="/books">Suchen</Nav.Link>
+            <Nav.Link as={Link} href="/create">Neues Buch</Nav.Link>
+          </Nav>
           <Button variant="outline-light">Logout</Button>
+        </Navbar.Collapse>
     </Navbar>
   );
 };
